db297dda83a8f9f3811bd6e2d86d078f51325277
rakuda-daraku <rakugaki.oukoku@gmail.com>
2017-10-25 16:35:03 +0900

    attributeを追加

--- sample1/src/main.js
+++ sample1/src/main.js
@@ -3,5 +3,6 @@ let L = require('leaflet');
 let map = L.map('map').setView([43.067656, 141.350857], 10);
 
 L.tileLayer('https://tile.mierune.co.jp/mierune_mono/{z}/{x}/{y}.png', {
+  attribution: 'Maptiles by <a href="http://mierune.co.jp/" target="_blank">MIERUNE</a>, under CC BY. Data by <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors, under ODbL.',
   maxZoom: 18
 }).addTo(map);
